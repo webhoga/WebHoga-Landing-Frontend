22405c1a514f1456f173052f8d7f09d30b4d62c9
WebHoga Digital Services <contact@webhoga.com>
2025-08-13 14:03:01 +0530

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
-import Router from './Router';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <Router />
-    </React.StrictMode>,
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+} else {
+    console.error('Root element not found. Make sure your index.html has a div with id="root"');
+}
