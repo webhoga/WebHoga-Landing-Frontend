46399f89e3ed366ae2dec6675b03506c00dbe9fe
WebHoga Digital Services <contact@webhoga.com>
2025-08-13 09:33:19 +0530

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// @ts-ignore
-import App from '/App.tsx';
+import App from './App';
 import './index.css';
 import Router from './Router';
 
