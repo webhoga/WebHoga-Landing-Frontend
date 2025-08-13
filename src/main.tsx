0fede8ed60dbe615c2e03c555d028429a0cc27b1
WebHoga Digital Services <contact@webhoga.com>
2025-08-13 09:30:56 +0530

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// @ts-ignore
+import App from '/App.tsx';
 import './index.css';
 import Router from './Router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        {/*<App />*/}
+        <App />
         <Router />
     </React.StrictMode>,
 );
